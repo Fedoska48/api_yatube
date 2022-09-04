7827557b0348f5725651dc2723d22656391090f7
Никита Федяев <itsfedos@gmail.com>
2022-09-04 12:41:46 +0300

    for 2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -11,7 +11,7 @@ router_v1 = DefaultRouter()
 router_v1.register(r'posts', PostViewSet)
 router_v1.register(r'groups', GroupViewSet)
 router_v1.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
-                basename='comment')
+                   basename='comment')
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
